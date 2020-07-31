ffbf23effacfa05ac33527a100cbbf709949dc7e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-31 18:58:28 +0000

    Automatically backed up by Learn

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,3 +3,25 @@ end
 
 
 #rails g controller admin dashboard stats financials settings --no-test-framework
+
+
+<<-HEREDOC 
+create  app/controllers/admin_controller.rb
+ route  get 'admin/settings'
+ route  get 'admin/financials'
+ route  get 'admin/stats'
+ route  get 'admin/dashboard'
+invoke  erb
+create    app/views/admin
+create    app/views/admin/dashboard.html.erb
+create    app/views/admin/stats.html.erb
+create    app/views/admin/financials.html.erb
+create    app/views/admin/settings.html.erb
+invoke  helper
+create    app/helpers/admin_helper.rb
+invoke  assets
+invoke    coffee
+create      app/assets/javascripts/admin.js.coffee
+invoke    scss
+create      app/assets/stylesheets/admin.css.scss
+HEREDOC
