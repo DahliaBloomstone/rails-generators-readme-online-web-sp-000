0314422a86e97395e2314bd7cbd06308f4bf26fe
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-31 18:57:24 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,6 @@ end
 
 # rails g model Author name:string genre:string bio:text --no-test-framework
 # invoke  active_record
-create    db/migrate/20190618010724_create_authors.rb
-create    app/models/application_record.rb
-create    app/models/author.rb
+# create    db/migrate/20190618010724_create_authors.rb
+# create    app/models/application_record.rb
+# create    app/models/author.rb
